77188b806bb00e15bd456c06f1d692b3f213ad5b
root <root@SL09V702.seb.com>
2018-07-10 10:16:09 +0200

    Ajout Function pour PatchPanel distant

--- wwwroot/inc/functions.php
+++ wwwroot/inc/functions.php
@@ -1190,6 +1190,13 @@ function getRSUforRow ($rowData,$espac)
 function testPatchPanel($dataRack)
 {
 	echo "<table border=0 cellspacing=0 cellpadding=3 width='100%'>\n";
+	echo "<table border=0 cellspacing=0 cellpadding=3 width='100%'>\n";
+	echo '<tr>'.
+			'<th width='.'50%'.'class=tdleft>'.
+				'<strong>PatchPanel de la baie :</strong>'.
+			'</th>'.
+			'<td class=tdleft><strong>PatchPanel distant :</strong></td>'.
+			'<td class=tdleft><strong>Ports utilisés :</strong></td></tr>';	
 	foreach ($dataRack['mountedObjects'] as $id)
 	{
 		$req=usePreparedSelectBlade
@@ -1202,56 +1209,94 @@ function testPatchPanel($dataRack)
 		if ($res[0]['objtype_id'] == 9)
 		{
 		$port_tot = listPortObj($id);
+		$port_used = listPortObj_used($id);
+		$PatchPdist = distPatchPanel($id);	
 		echo '<tr>'.
 			'<th width='.'50%'.'class=tdleft>'.
 				'<a href =index.php?page=object&object_id='.$id.'>'.$res[0]['name'].'</a>'.
 			'</th>'.
-			'<td class=tdleft>vers baie...</td>'.
-			'<td class=tdleft>Ports Restant : nbr / '.$port_tot.'</td></tr>';	
+			'<td class=tdleft>'.	
+				'<strong><a href =index.php?page=object&object_id='.$id.'>'.$PatchPdist.'</a></strong>'.
+			'</td>'.
+			'<td class=tdleft>'.$port_used.' / '.$port_tot.'</td></tr>';	
 		//echo '<p><strong><a href ="index.php?page=object&object_id='.$id.'">'.$res[0]['name'].'</a></strong></p>';
-		print_r($neyme);
+	//	print_r($neyme);
 		}
 	}
 	echo "</table>\n";
 }
-
-function listPortObj ($obj_id)
+function listPortObj_used ($obj_id)
 {
-		//echo '<br>';
-		//echo 'Id obj : '.$obj_id.'';
-
-		$req=usePreparedSelectBlade
+	$req=usePreparedSelectBlade
  		(
 			'SELECT porta,portb,id '. 
-			'FROM LinkBackend lbe '.
-			'JOIN Port p ON lbe.porta=p.id OR lbe.portb=p.id '.
+			'FROM Link l '.
+			'JOIN Port p ON l.porta=p.id OR l.portb=p.id '.
 			'WHERE p.object_id='.$obj_id.';'
 		);
 		
-		$res = $req->fetchAll();
-		$nbrport=count($res);
-		
-		/*for ( $i = 0 ; $i <= $nbrport-1 ; $i++)
-		{
-			echo '<br>';
-			echo ' Porta : '.$res[$i]['porta'].'';
-			echo ' Portb : '.$res[$i]['portb'].'';
-			echo ' id : '.$res[$i]['id'].'';
-			echo '</br>';
-			if($res[$i]['porta'] == $res[$i]['id'])
-			{
-				$counter++;
-			}
-			if($res[$i]['portb'] == $res[$i]['id'])
-			{
-				$counter++;
-			}	
-		}*/
-		//echo '<br>';
-		//echo 'Le nombre est de '.$nbrport.'';
-		//echo '</br>';
-return $nbrport ;
+	$res = $req->fetchAll();
+	$portsUsed=count($res);
+	return $portsUsed ;
+}
+function listPortObj ($obj_id)
+{
+	$req=usePreparedSelectBlade
+ 	(
+		'SELECT porta,portb,id '. 
+		'FROM LinkBackend lbe '.
+		'JOIN Port p ON lbe.porta=p.id OR lbe.portb=p.id '.
+		'WHERE p.object_id='.$obj_id.';'
+	);
+	
+	$res = $req->fetchAll();
+	$nbrport=count($res);
+	return $nbrport ;
 }	
+
+//////////////////////////////////////////////////////////////////////
+/*     Nouvelle Fonction permettant de déterminer le nom du 	    */
+/*     PatchPanel distant 				 	    */
+function distPatchPanel ($obj_id)
+{
+	$req=usePreparedSelectBlade
+	(
+		'SELECT porta,portb,id '. 
+		'FROM LinkBackend lbe '.
+		'JOIN Port p ON lbe.porta=p.id OR lbe.portb=p.id '.
+		'WHERE p.object_id='.$obj_id.';'
+	);
+	
+	$res = $req->fetchAll();
+	
+	if($res[0]['porta'] == $res[0]['id']){
+		if (isset($res[0]['portb'])){
+			$req=usePreparedSelectBlade
+			(
+			   'SELECT o.name '. 
+			   'FROM Object o '.
+			   'JOIN Port p ON o.id=p.object_id '.
+			   'WHERE p.id='.$res[0]['portb'].';'
+		   	);
+		   
+		   	$res2 = $req->fetchAll();
+		   	return $res2[0]['name'];
+		}
+	}else if ($res[0]['portb'] == $res[0]['id']){
+		if (isset ($res[0]['porta'])){
+			$req=usePreparedSelectBlade
+			(
+			   'SELECT o.name '. 
+			   'FROM Object o '.
+			   'JOIN Port p ON o.id=p.object_id '.
+			   'WHERE p.id='.$res[0]['porta'].';'
+			);
+			   
+			$res2 = $req->fetchAll();
+			return $res2[0]['name'];
+		}
+	}
+}
 //////////////////////////////////////////////////////////////////////
 /*     Nouvelle Fonction permettant la coloration des U réservés    */
 function reservedColor ($id_obj)
